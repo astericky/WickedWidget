6a6024fdbe2a2c701f809f72189c572868367921
Chris Sanders <astericky@gmail.com>
2019-12-13 13:12:48 -0800

    WW: add appointment list component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppointmentListComponent from './component/AppointmentListComponent/AppointmentListComponent';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AppointmentListComponent />
     </div>
   );
 }
--- src/component/AppointmentListComponent/AppointmentListComponent.jsx
+++ src/component/AppointmentListComponent/AppointmentListComponent.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+// this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
+/** @jsx jsx */
+// import React from 'react';
+import moment from 'moment-timezone';
+
+import { jsx } from '@emotion/core';
+import styled from '@emotion/styled';
+
+import PlushCareLogo from '../../logo.png';
+
 
 function AppointmentListComponent() {
+  let date = moment().format('MM/DD/YYYY');
   return (
     <div className="plushcare-widget">
-      <header className="plushcare-widget-header">
-        <h1 className="plushcare-widget-heading">PlushCare</h1>
+      <header>
+        <h1>
+          <a href="https://plushcare.com/book">
+            <img src={PlushCareLogo} alt="PlushCare Logo" />
+            PlushCare
+          </a>
+        </h1>
       </header>
-      <div className="date-filter"></div>
+      <div className="date-filter">
+        <input type="date" value={date} />
+      </div>
 
     </div>
   );
 }
 
-return AppointmentListComponent
+const styledAppointmentListComponent = styled(AppointmentListComponent)`
+  
+`
+
+export default styledAppointmentListComponent;
 
 
