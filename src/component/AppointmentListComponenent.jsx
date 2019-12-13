2a97619ae43affc72d2f9852e1b6e0a86369fa57
Chris Sanders <astericky@gmail.com>
2019-12-13 12:37:37 -0800

    WW: add appointment list component

---
+++ src/component/AppointmentListComponenent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function AppointmentListComponent() {
+  return (
+    <div className="plushcare-widget">
+      <header className="plushcare-widget-header">
+        <h1 className="plushcare-widget-heading">PlushCare</h1>
+      </header>
+      <div className="date-filter"></div>
+
+    </div>
+  );
+}
+
+return AppointmentListComponent
+
+
