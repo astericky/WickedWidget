3e9a8d81dbd38cb294391242ba4e135b74fe1f29
Chris Sanders <astericky@gmail.com>
2019-12-13 13:46:50 -0800

    WW remove header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import AppointmentListComponent from './component/AppointmentListComponent/AppointmentListComponent';
@@ -28,20 +27,6 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       {renderSwitch(display)}
     </div>
   );
--- src/component/AppointmentListComponent/AppointmentListComponent.jsx
+++ src/component/AppointmentListComponent/AppointmentListComponent.jsx
@@ -1,7 +1,6 @@
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsx jsx */
 // import moment from 'moment-timezone';
-
 import { jsx } from '@emotion/core';
 import styled from '@emotion/styled';
 
@@ -10,18 +9,18 @@ import AppointmentListDateFilter from './AppointmentListDateFilter';
 
 
 function AppointmentListComponent() {
-  
   return (
     <div className="plushcare-widget">
       <AppointmentListComponentHeader />
       <AppointmentListDateFilter />
-
     </div>
   );
 }
 
 const styledAppointmentListComponent = styled(AppointmentListComponent)`
-
+  width: 300px;
+  height: 250px;
+  overflow-y: scroll;
 `
 
 export default styledAppointmentListComponent;
