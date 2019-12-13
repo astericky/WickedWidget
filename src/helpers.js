8842bb527d48059620093c01ed209ee391a3fded
Derry Nixon <nixondc93@gmail.com>
2019-12-13 13:12:00 -0800

    requests boiler plate

---
+++ src/helpers.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'https://api-dev.plushcare.com/';
+const HEADERS = {
+
+}
+
+const fetchAppointments = async (state, count=3) => (
+    await axios.get(url, headers=HEADERS, data=json.sy{})
+);
+
+const createUser = async () => (
+    await axios.post()
+);
+
+const submitPayment = async () => (
+    await axios.post()
+);
+
+const submitInsurance = async () => (
+    await axios.post()
+);
